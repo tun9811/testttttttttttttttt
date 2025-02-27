b03dd8672471fe66c3ea1c5211c6bcd11b4b90a7
x3sharkx2z <bevz91741@gmail.com>
2025-02-27 19:51:09 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811/testtttttttttttttttbloxfruit/refs/heads/main/script.lua"))()
+else
+  game.Players.LocalPlayer:kick("ไม่พบแมพ")
 end
