ba9bc514f6615a7a7e665eb7a5f582b3d5e26b8e
x3sharkx2z <bevz91741@gmail.com>
2025-02-27 19:50:02 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,3 @@
-print("hi)
+if game.PlaceId == 2753915549 or 4442272183 or 7449423635 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811/testtttttttttttttttbloxfruit/refs/heads/main/script.lua"))()
+end
